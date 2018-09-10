5fca79332c56c5fdccf66bcdff6e78e707d7f921
MasaoBlue <masaoki62@gmail.com>
2018-09-10 23:38:53 +0900

    angularからwebsocket接続した際にCORSエラーになるのを修正

--- sit-cv-app/src/main/java/org/sitoolkit/design/infra/websocket/WebSocketConfig.java
+++ sit-cv-app/src/main/java/org/sitoolkit/design/infra/websocket/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:4200").withSockJS();
     }
 }
