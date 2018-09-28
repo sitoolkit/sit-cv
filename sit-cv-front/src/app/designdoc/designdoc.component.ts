ca6f4c8d700fe7d6cd1d628abe2f5164a7fc9c42
kfujinuma <40159739+kfujinuma@users.noreply.github.com>
2018-09-28 13:53:42 +0900

    Feature/tree view of designdoc (#12)

--- sit-cv-front/src/app/designdoc/designdoc.component.ts
+++ sit-cv-front/src/app/designdoc/designdoc.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 import * as Stomp from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+
+class Node {
+  children: Node[] = [];
+  name: string;
+  designDocId: string;
+}
 
 @Component({
   templateUrl: './designdoc.component.html',
@@ -12,6 +20,9 @@ export class DesignDocComponent {
   currentDesignDocId = '';
   currentDiagrams = [];
   objectKeys = Object.keys;
+
+  nestedTreeControl = new NestedTreeControl<Node>((node: Node) => node.children);
+  nestedDataSource = new MatTreeNestedDataSource();
 
   constructor() {
     this.connect();
@@ -41,6 +52,7 @@ export class DesignDocComponent {
 
   renderDesingDocList(designDocIds) {
     this.designDocIds = designDocIds;
+    this.nestedDataSource.data = this.createTree(designDocIds);
   }
 
   renderDiagrams(diagrams) {
@@ -55,6 +67,54 @@ export class DesignDocComponent {
     this.subscribe(this.currentDesignDocId);
     this.stompClient.send("/app/designdoc/detail", {}, this.currentDesignDocId);
     return false;
+  }
+
+  hasNestedChild = (_: number, nodeData: Node) => !nodeData.designDocId;
+
+  createTree(designDocIds: string[]): Node[] {
+    const nodes: Node[] = [];
+    designDocIds.forEach(designDocId => {
+      this.merge(nodes, [this.createNodeByDocId(designDocId)]);
+    });
+    return nodes;
+  }
+
+  createNodeByDocId(designDocId: string): Node {
+    const pos = designDocId.search(/[^.]*\(.*/);
+    const inners = designDocId.substr(0, pos - 1).split('.');
+    const leaf = designDocId.substr(pos);
+
+    let rootNode: Node = null;
+    let parentNode: Node = null;
+
+    inners.forEach(packageOrClass => {
+      const creatingNode = new Node();
+      creatingNode.name = packageOrClass;
+      if (parentNode != null) {
+        parentNode.children.push(creatingNode);
+      } else {
+        rootNode = creatingNode;
+      }
+      parentNode = creatingNode;
+    });
+
+    const leafNode = new Node();
+    leafNode.name = leaf;
+    leafNode.designDocId = designDocId;
+    parentNode.children.push(leafNode);
+
+    return rootNode;
+  }
+
+  merge(into: Node[], merging: Node[]) {
+    merging.forEach(node => {
+      const matched = into.filter(n => n.name === node.name);
+      if (matched.length === 0) {
+        into.push(node);
+      } else {
+        this.merge(matched[0].children, node.children);
+      }
+    });
   }
 
 }
