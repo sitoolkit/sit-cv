9221e08f4d6d023aba5d1b141b7a4d1dce917772
Kazunari Fujinuma <monocrea.fujinuma@DESKTOP-20M0B3C>
2018-10-15 17:29:31 +0900

    Improve front appearance

---
+++ sit-cv-front/src/app/pipe/hide-package.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HidePackagePipe } from './hide-package.pipe';
+
+describe('HidePackagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new HidePackagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
