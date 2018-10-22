8889065cfdea14a5f5fe9e115cbc69095a1214a1
MasaoBlue <masao.hirocho@monocrea.jp>
2018-10-22 14:59:16 +0900

    レポート出力後のindex.htmlが起動できなかったのを修正

--- sit-cv-front/src/app/srv/designdoc/designdoc-report.service.ts
+++ sit-cv-front/src/app/srv/designdoc/designdoc-report.service.ts
@@ -3,23 +3,32 @@ import { DesignDocService } from './designdoc.service';
 import { DesignDocIdList } from './designdoc-id-list';
 import { DesignDocDetail } from './designdoc-detail';
 import { DesignDocReportRepository } from './designdoc-report.repository';
+import { AsyncSubject } from 'rxjs';
 
 @Injectable()
 export class DesignDocReportService implements DesignDocService {
+
+  private idListSubject: AsyncSubject<boolean> = new AsyncSubject<boolean>();
 
   constructor(private repository: DesignDocReportRepository) {}
 
   getIdList(
     callback: (idList: DesignDocIdList) => void
   ): void {
-    this.repository.getIdList(callback);
+    this.repository.getIdList((idList: DesignDocIdList) => {
+      callback(idList);
+      this.idListSubject.next(true);
+      this.idListSubject.complete();
+    });
   }
 
   getDetail(
     designDocId: string,
     callback: (detail: DesignDocDetail) => void
   ): void {
-    this.repository.getDetail(designDocId, callback);
+    this.idListSubject.subscribe(() => {
+      this.repository.getDetail(designDocId, callback);
+    })
   }
 
 }
