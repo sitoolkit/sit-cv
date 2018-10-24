2d17d52cb047b72fc6933b6a5f17dd5bcb7228b6
MasaoBlue <masao.hirocho@monocrea.jp>
2018-10-24 15:26:06 +0900

    Output report with mvn command

---
+++ sit-cv-maven-plugin/src/main/java/io/sitoolkit/cv/plugin/maven/ReportMojo.java
@@ -0,0 +1,24 @@
+package io.sitoolkit.cv.plugin.maven;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import io.sitoolkit.cv.core.app.config.ServiceFactory;
+
+@Mojo(name = "report")
+public class ReportMojo extends AbstractMojo {
+
+    @Parameter(property = "target.project", defaultValue = ".")
+    private String[] projects;
+
+    @Override
+    public void execute() {
+        Path projectDir = Paths.get(projects[0]);
+        ServiceFactory.initialize(projectDir).getReportService().export();
+    }
+
+}
