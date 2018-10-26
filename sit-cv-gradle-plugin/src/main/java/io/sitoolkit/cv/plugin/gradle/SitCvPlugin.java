11fd18fcba0f48b400f441161d8910e928bb084d
MasaoBlue <masao.hirocho@monocrea.jp>
2018-10-26 14:42:29 +0900

    change plugin classname

--- sit-cv-gradle-plugin/src/main/java/io/sitoolkit/cv/plugin/gradle/SitCvPlugin.java
+++ sit-cv-gradle-plugin/src/main/java/io/sitoolkit/cv/plugin/gradle/SitCvPlugin.java
@@ -3,11 +3,13 @@ package io.sitoolkit.cv.plugin.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-public class PluginManager implements Plugin<Project> {
+public class SitCvPlugin implements Plugin<Project> {
+    private static final String GROUP_NAME = "SIT-CV";
+
     @Override
     public void apply(Project project) {
         project.getTasks().create("cvReport", ReportTask.class, (task) -> {
-            task.setGroup("sit-cv");
+            task.setGroup(GROUP_NAME);
             task.setDescription("Export designdocs from source code");
         });
     }
