072e7f57c3d1843fc091810ea9cb9e98ca84b7d2
MasaoBlue <masao.hirocho@monocrea.jp>
2018-10-26 18:01:10 +0900

    add gradle plugin test

--- sit-cv-gradle-plugin/src/main/java/io/sitoolkit/cv/plugin/gradle/ReportTask.java
+++ sit-cv-gradle-plugin/src/main/java/io/sitoolkit/cv/plugin/gradle/ReportTask.java
@@ -1,20 +1,19 @@
 package io.sitoolkit.cv.plugin.gradle;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.tasks.options.Option;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.options.Option;
 
 import io.sitoolkit.cv.core.app.config.ServiceFactory;
 
 public class ReportTask extends DefaultTask {
-    private Path basePath = Paths.get(System.getProperty("user.dir"));
+    private Path basePath = getProject().getProjectDir().toPath();
 
     @Input
     private List<Path> projectDirs;
---
+++ sit-cv-gradle-plugin/src/test/java/io/sitoolkit/cv/plugin/gradle/ReportTaskTest.java
@@ -0,0 +1,39 @@
+package io.sitoolkit.cv.plugin.gradle;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+
+import org.apache.commons.io.FileUtils;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ReportTaskTest {
+    @Rule
+    public final TemporaryFolder tempDir = new TemporaryFolder();
+
+    @Test
+    public void testExport() {
+        URL testURL = getClass().getClassLoader().getResource("test-report-project");
+        File testDir = new File(testURL.getFile());
+        File projectDir = null;
+        try {
+            projectDir = tempDir.newFolder("report");
+            FileUtils.copyDirectory(testDir, projectDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        GradleRunner.create()
+                .withProjectDir(projectDir)
+                .withArguments(Arrays.asList("cvReport"))
+                .build();
+
+        assertTrue(new File(projectDir, "docs").exists());
+    }
+}
