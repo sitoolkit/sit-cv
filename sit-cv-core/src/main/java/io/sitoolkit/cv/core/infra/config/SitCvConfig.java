8bde7037615f59072f754076f74fc4f25dcc4d11
MasaoBlue <masao.hirocho@monocrea.jp>
2018-10-29 13:44:04 +0900

    remove unnecessary code

--- sit-cv-core/src/main/java/io/sitoolkit/cv/core/infra/config/SitCvConfig.java
+++ sit-cv-core/src/main/java/io/sitoolkit/cv/core/infra/config/SitCvConfig.java
@@ -8,10 +8,8 @@ import java.nio.file.Path;
 import io.sitoolkit.cv.core.infra.util.JsonUtils;
 import io.sitoolkit.cv.core.infra.util.SitResourceUtils;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
-@Slf4j
 public class SitCvConfig {
 
     private static final String CONFIG_FILE_NAME = "sit-cv-config.json";
@@ -24,11 +22,6 @@ public class SitCvConfig {
         Path jsonFile = baseDir.resolve(CONFIG_FILE_NAME);
 
         try {
-            log.info("exists " + jsonFile.toFile().exists());
-            log.info("existspath1 " + jsonFile.toFile());
-            log.info("existspath2 " + jsonFile.toFile().toURI());
-            log.info("existspath3 " + jsonFile.toAbsolutePath().normalize());
-
             String json = jsonFile.toFile().exists() ? new String(Files.readAllBytes(jsonFile))
                     : SitResourceUtils.res2str(SitCvConfig.class, CONFIG_FILE_NAME);
             return JsonUtils.str2obj(json, SitCvConfig.class);
