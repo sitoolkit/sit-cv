3c1d47a47735de4ca1629a3f48dd2ef477bbb0dc
MasaoBlue <16271994+MasaoBlue@users.noreply.github.com>
2018-10-29 15:50:03 +0900

    Feature/gradle report (#37)

---
+++ sit-cv-gradle-plugin/src/main/java/io/sitoolkit/cv/plugin/gradle/SitCvPlugin.java
@@ -0,0 +1,16 @@
+package io.sitoolkit.cv.plugin.gradle;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class SitCvPlugin implements Plugin<Project> {
+    private static final String GROUP_NAME = "SIT-CV";
+
+    @Override
+    public void apply(Project project) {
+        project.getTasks().create("cvReport", ReportTask.class, (task) -> {
+            task.setGroup(GROUP_NAME);
+            task.setDescription("Export designdocs from source code");
+        });
+    }
+}
