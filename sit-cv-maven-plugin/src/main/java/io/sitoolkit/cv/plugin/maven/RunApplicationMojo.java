4f01ea69ef8279cfb645fa9ae443444038705c5a
Yuichi Kuwhara <yuichi.kuwahara@monocrea.jp>
2018-11-24 18:54:22 +0900

    Modify execution of maven plugin server mode

--- sit-cv-maven-plugin/src/main/java/io/sitoolkit/cv/plugin/maven/RunApplicationMojo.java
+++ sit-cv-maven-plugin/src/main/java/io/sitoolkit/cv/plugin/maven/RunApplicationMojo.java
@@ -1,163 +1,48 @@
 package io.sitoolkit.cv.plugin.maven;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import io.sitoolkit.cv.core.infra.SitRepository;
-import io.sitoolkit.util.buidtoolhelper.maven.MavenProject;
-import io.sitoolkit.util.buidtoolhelper.process.ProcessCommand;
-import io.sitoolkit.util.buidtoolhelper.process.ProcessConversation;
-import io.sitoolkit.util.buidtoolhelper.process.ProcessExitCallback;
-import io.sitoolkit.util.buidtoolhelper.process.StdoutListener;
+import io.sitoolkit.cv.app.SitCvApplication;
 
 @Mojo(name = "run")
-public class RunApplicationMojo
-        extends AbstractMojo {
-
-    @Parameter(defaultValue = "${plugin.groupId}")
-    String cvAppGroupId;
-
-    @Parameter(defaultValue = "sit-cv-app")
-    String cvAppArtifactId;
-
-    @Parameter(defaultValue = "${plugin.version}")
-    String cvAppVersion;
-
-    @Parameter(defaultValue = "30")
-    int cvAppStartWaitingSec;
+public class RunApplicationMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project.basedir}")
     File projectDir;
 
-    static final int APP_START_LISTENING_INTERVAL_MILLI = 2000;
+    @Parameter(defaultValue = "x")
+    String stopKey;
 
     @Override
-    public void execute()
-            throws MojoExecutionException {
-                Path executableJar = prepareExecutableJar();
-                startApp(executableJar);
-    }
+    public void execute() throws MojoExecutionException {
 
-    Path prepareExecutableJar() throws MojoExecutionException {
-
-        Path repository = SitRepository.getRepositoryPath().resolve("sit-cv").resolve(cvAppArtifactId);
-        String jarName = String.format("%s-%s.jar", cvAppArtifactId, cvAppVersion);
-        String artifact = String.format("%s:%s:%s", cvAppGroupId, cvAppArtifactId, cvAppVersion);
-
-        StdoutListener listener = new StdoutListener() {
-            @Override
-            public void nextLine(String line) {
-                getLog().debug("mvnw copy >>" + line);
-            }
-        };
-
-        getLog().info("copying SIT-CV-App jar file...");
-
-        MavenProject.load(projectDir.toPath())
-                .mvnw("dependency:copy",
-                        "-DoutputDirectory=" + repository.toAbsolutePath().toString(),
-                        "-Dartifact=" + artifact)
-                .stdout(listener)
-                .execute();
-
-        Path jarPath = repository.resolve(jarName);
-
-        if (Files.exists(jarPath)) {
-            getLog().info("SIT-CV-App jar file copyed");
-            return jarPath;
-
-        } else {
-            getLog().error("copying SIT-CV-App jar file failed");
-            throw new MojoExecutionException("copying SIT-CV-App jar file failed");
+        List<String> args = new ArrayList<>();
+        if (projectDir != null) {
+            args.add("--project");
+            args.add(projectDir.getAbsolutePath());
         }
-    }
+        SitCvApplication.main(args.toArray(new String[args.size()]));
 
-    void startApp(Path executableJar) throws MojoExecutionException {
+        getLog().info("Press " + stopKey + " and enter to stop server");
 
-        CvAppListener listener = new CvAppListener();
-        ProcessCommand sitCommand = new ProcessCommand()
-                .currentDirectory(projectDir.toPath())
-                .command("java")
-                .args("-jar",
-                        executableJar.toAbsolutePath().toString())
-                .stdout(listener);
-        sitCommand.getExitCallbacks().add(listener);
-
-        getLog().info("Starting SIT-CV-App ...");
-        ProcessConversation processConversation = sitCommand.executeAsync();
-        Instant executedTime = Instant.now();
-
-        while (listener.getAppState() == AppState.STARTING &&
-                Instant.now().isBefore(executedTime.plusSeconds(cvAppStartWaitingSec))) {
-            try {
-                TimeUnit.MILLISECONDS.sleep(APP_START_LISTENING_INTERVAL_MILLI);
-            } catch (InterruptedException e) {
-                throw new MojoExecutionException("Waiting app start is interrupted", e);
-            }
-        }
-
-        switch (listener.getAppState()) {
-        case STARTED:
-            getLog().info("SIT-CV-App started");
-            return;
-
-        case STARTING:
-            getLog().error("SIT-CV-App start failed : timeout - " + cvAppStartWaitingSec + " seconds");
-            shutdown(processConversation);
-            throw new MojoExecutionException("SIT-CV-App start failed : timeout");
-
-        case EXITED:
-            getLog().error("SIT-CV-App start failed : exit code = " + listener.exitCode);
-            throw new MojoExecutionException("SIT-CV-App start failed");
-        }
-    }
-
-    void shutdown(ProcessConversation processConversation) throws MojoExecutionException{
-        getLog().info("Shutting down SIT-CV-App ...");
-        processConversation.getProcess().destroyForcibly();
-        processConversation.destroy();
-        getLog().info("SIT-CV-App was shut down");
-    }
-
-    class CvAppListener implements StdoutListener, ProcessExitCallback {
-        boolean isAppStarted = false;
-        boolean isAppExited = false;
-        int exitCode = 0;
-
-        @Override
-        public void nextLine(String line) {
-            if (!isAppStarted) {
-                getLog().debug("starting SIT-CV-App >>" + line);
-                if (line.contains("Started SitCvApplication")) {
-                    getLog().debug("SIT-CV Started");
-                    isAppStarted = true;
+        if (StringUtils.isNotEmpty(stopKey)) {
+            try (Scanner scanner = new Scanner(System.in)) {
+                while (true) {
+                    if (StringUtils.equalsIgnoreCase(stopKey, scanner.nextLine())) {
+                        break;
+                    }
                 }
             }
         }
-
-        @Override
-        public void callback(int exitCode) {
-            getLog().debug("SIT-CV-App Exited");
-            isAppExited = true;
-            this.exitCode = exitCode;
-        }
-
-        public AppState getAppState() {
-            return isAppExited ? AppState.EXITED
-                    : isAppStarted ? AppState.STARTED
-                            : AppState.STARTING;
-        }
-    };
-
-    enum AppState {
-        STARTING, STARTED, EXITED
     }
+
 }
--- sit-cv-maven-plugin/src/test/java/io/sitoolkit/cv/plugin/maven/RunApplicationMojoTest.java
+++ sit-cv-maven-plugin/src/test/java/io/sitoolkit/cv/plugin/maven/RunApplicationMojoTest.java
@@ -1,105 +1,27 @@
 package io.sitoolkit.cv.plugin.maven;
 
-
-import static org.junit.Assert.*;
-
 import java.io.File;
 
-import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.testing.MojoRule;
-import org.apache.maven.plugin.testing.WithoutMojo;
+import org.apache.maven.plugin.testing.resources.TestResources;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-public class RunApplicationMojoTest
-{
-    @Rule
-    public MojoRule rule = new MojoRule()
-    {
-        @Override
-        protected void before() throws Throwable
-        {
-        }
-
-        @Override
-        protected void after()
-        {
-        }
-    };
+public class RunApplicationMojoTest {
 
     @Rule
-    public ExpectedException expectedException = ExpectedException.none();
+    public MojoRule mojoRule = new MojoRule();
 
-    /**
-     * @throws Exception if any
-     */
-    @Test()
-    public void testJarCopyFailure()
-            throws Exception
-    {
-        expectedException.expect(MojoExecutionException.class);
-        expectedException.expectMessage("copying SIT-CV-App jar file failed");
+    @Rule
+    public TestResources resources = new TestResources();
 
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
-
-        RunApplicationMojo mojo = ( RunApplicationMojo ) rule.lookupConfiguredMojo( pom, "run" );
-        mojo.cvAppArtifactId = "aaaaaaa";
-        assertNotNull( mojo );
-        mojo.execute();
-
-    }
-
-    /**
-     * @throws Exception if any
-     */
-    @Test()
-    public void testTimeOut()
-            throws Exception
-    {
-        expectedException.expect(MojoExecutionException.class);
-        expectedException.expectMessage("SIT-CV-App start failed : timeout");
-
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
-
-        RunApplicationMojo mojo = ( RunApplicationMojo ) rule.lookupConfiguredMojo( pom, "run" );
-        mojo.cvAppStartWaitingSec = 1;
-        assertNotNull( mojo );
-        mojo.execute();
-
-    }
-
-    /**
-     * @throws Exception if any
-     */
-    @Test()
-    public void testNormal()
-            throws Exception
-    {
-        File pom = new File( "target/test-classes/project-to-test/" );
-        assertNotNull( pom );
-        assertTrue( pom.exists() );
-
-        RunApplicationMojo mojo = ( RunApplicationMojo ) rule.lookupConfiguredMojo( pom, "run" );
-        assertNotNull( mojo );
-        try {
-            mojo.execute();
-        } catch (MojoExecutionException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /** Do not need the MojoRule. */
-    @WithoutMojo
     @Test
-    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn()
-    {
-        assertTrue( true );
+    public void testMojoGoal() throws Exception {
+        File pom = new File(resources.getBasedir("test-project-1"), "pom.xml");
+
+        Mojo mojo = mojoRule.lookupMojo("run", pom);
+        mojo.execute();
     }
 
 }
-
