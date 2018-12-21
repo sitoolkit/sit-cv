f12aedb71443b3c8249149b7e8faef10decffee0
MasaoBlue <16271994+MasaoBlue@users.noreply.github.com>
2018-12-21 17:26:16 +0900

    Feature/diagram enter animation (#66)

--- sit-cv-front/src/app/cmp/designdoc/designdoc.component.ts
+++ sit-cv-front/src/app/cmp/designdoc/designdoc.component.ts
@@ -1,37 +1,55 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import * as $ from 'jquery';
 import { DesignDocService } from '../../srv/designdoc/designdoc.service';
 import { ApiDocComponent } from './apidoc/apidoc.component';
+import { trigger, style, transition, animate } from '@angular/animations';
 
-class Diagram {
+interface Diagram {
   diagram: SafeHtml;
   width: string;
   heightRatio: string;
+}
+
+interface DiagramGroup {
+  diagrams: Diagram[];
 }
 
 @Component({
   selector: 'app-designdoc',
   templateUrl: './designdoc.component.html',
   styleUrls: ['./designdoc.component.css'],
+  animations: [
+    trigger('diagramAnimation', [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+      transition("leaveEnable => void", [
+        animate(300, style({ opacity: 0 }))
+      ])
+    ]),
+  ],
 })
 
 export class DesignDocComponent implements OnInit {
   designDocIds = [];
   currentDesignDocId = '';
-  currentDiagrams = [];
+  currentDiagramGroups: DiagramGroup[] = [];
   objectKeys = Object.keys;
   diagramApiDocs = {};
   selectedMethodSignatures = [];
   currentMethodSignature = "";
   isDiagramLoading = false;
+  isLeaveAnimationEnabled = true;
 
   constructor(
     private route: ActivatedRoute,
     private sanitizer: DomSanitizer,
     public snackBar: MatSnackBar,
+    private chRef: ChangeDetectorRef,
     @Inject('DesignDocService') private ddService: DesignDocService) { }
 
   ngOnInit() {
@@ -53,13 +71,16 @@ export class DesignDocComponent implements OnInit {
         heightRatio: (svg.height() / svg.width() * 100) + '%',
       });
     });
-    this.currentDiagrams = trustDiagrams;
+    this.currentDiagramGroups[0] = { diagrams: trustDiagrams };
   }
 
   showDesignDocDetail(designDocId) {
     if (this.currentDesignDocId) {
-      this.currentDiagrams = [];
       this.selectedMethodSignatures = [];
+      this.isLeaveAnimationEnabled = false;
+      this.chRef.detectChanges();
+      this.currentDiagramGroups = [];
+      this.isLeaveAnimationEnabled = true;
     }
     this.currentDesignDocId = designDocId;
     this.isDiagramLoading = true;
