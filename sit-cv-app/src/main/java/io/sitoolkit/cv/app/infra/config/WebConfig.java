5ebbbe4ec73873d7f87b0748dd178ad704d0f546
MasaoBlue <masao.hirocho@monocrea.jp>
2019-01-17 18:25:10 +0900

    Adjust border width

--- sit-cv-app/src/main/java/io/sitoolkit/cv/app/infra/config/WebConfig.java
+++ sit-cv-app/src/main/java/io/sitoolkit/cv/app/infra/config/WebConfig.java
@@ -1,16 +1,18 @@
 package io.sitoolkit.cv.app.infra.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    ApplicationConfig config;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins(config.getAllowedOrigins());
     }
 }
