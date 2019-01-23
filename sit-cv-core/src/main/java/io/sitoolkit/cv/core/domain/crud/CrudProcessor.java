dce48a5f926f322490e3d5020de92d6ccae5cda9
MasaoBlue <masao.hirocho@monocrea.jp>
2019-01-23 18:07:43 +0900

    Split the process of find repository crudrow

--- sit-cv-core/src/main/java/io/sitoolkit/cv/core/domain/crud/CrudProcessor.java
+++ sit-cv-core/src/main/java/io/sitoolkit/cv/core/domain/crud/CrudProcessor.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 
 import io.sitoolkit.cv.core.domain.classdef.ClassDef;
+import io.sitoolkit.cv.core.domain.classdef.MethodCallDef;
 import io.sitoolkit.cv.core.domain.tabledef.TableDef;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -52,26 +53,14 @@ public class CrudProcessor {
             entryPoint.getMethods().stream().forEach(entryPointMethod -> {
                 entryPointMethod.getMethodCallsRecursively().forEach(methodCalledByEntryPoint -> {
 
-                    CrudRow repositoryMethodCrudRow = null;
+                    Optional<CrudRow> foundRow = findRepositoryMethodCrudRow(
+                            repositoryMethodMatrix, methodCalledByEntryPoint);
 
-                    if (methodCalledByEntryPoint.getClassDef() != null
-                            && methodCalledByEntryPoint.getClassDef().isInterface()) {
-
-                        Optional<CrudRow> crudRow = methodCalledByEntryPoint.getClassDef()
-                                .getKnownImplClasses().stream().map((classDef) -> {
-                                    String signature = classDef.getFullyQualifiedName() + "."
-                                            + methodCalledByEntryPoint.getSignature();
-                                    return repositoryMethodMatrix.getCrudRowMap().get(signature);
-                                }).filter(Objects::nonNull).findFirst();
-                        repositoryMethodCrudRow = crudRow.orElse(null);
-                    } else {
-                        repositoryMethodCrudRow = repositoryMethodMatrix.getCrudRowMap()
-                                .get(methodCalledByEntryPoint.getQualifiedSignature());
-                    }
-
-                    if (repositoryMethodCrudRow == null) {
+                    if (!foundRow.isPresent()) {
                         return;
                     }
+
+                    CrudRow repositoryMethodCrudRow = foundRow.get();
 
                     CrudRow entryPointMethodCrudRow = result.getCrudRowMap()
                             .computeIfAbsent(entryPointMethod.getQualifiedSignature(), (key) -> {
@@ -89,6 +78,24 @@ public class CrudProcessor {
         });
 
         return result;
+    }
+
+    private Optional<CrudRow> findRepositoryMethodCrudRow(CrudMatrix repositoryMethodMatrix,
+            MethodCallDef methodCall) {
+
+        if (methodCall.getClassDef() != null && methodCall.getClassDef().isInterface()) {
+
+            Optional<CrudRow> crudRow = methodCall.getClassDef().getKnownImplClasses().stream()
+                    .map((classDef) -> {
+                        String signature = classDef.getFullyQualifiedName() + "."
+                                + methodCall.getSignature();
+                        return repositoryMethodMatrix.getCrudRowMap().get(signature);
+                    }).filter(Objects::nonNull).findFirst();
+            return crudRow;
+        } else {
+            return Optional.of(
+                    repositoryMethodMatrix.getCrudRowMap().get(methodCall.getQualifiedSignature()));
+        }
     }
 
 }
