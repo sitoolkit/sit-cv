c47a899f42ec1f96bc48c92df0a3fbc33c08c74c
MasaoBlue <masao.hirocho@monocrea.jp>
2019-06-07 09:41:28 +0900

    Create new vue project

---
+++ sit-cv-front-vue/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <div id="nav">Code Visualizer</div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="less">
+</style>
---
+++ sit-cv-front-vue/src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">empty content</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {}
+</script>
