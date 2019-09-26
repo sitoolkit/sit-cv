50c4fe210bf461b2234f9b0184c3901e2e2ecc0c
kfujinuma <40159739+kfujinuma@users.noreply.github.com>
2019-09-26 15:38:58 +0900

    Feature/lifeline only (#101)

---
+++ sample/src/main/java/a/b/c/ASpecification.java
@@ -0,0 +1,21 @@
+package a.b.c;
+
+import java.util.List;
+
+public class ASpecification {
+
+    public boolean isSatisfiedByList(List<XEntity> targetList) {
+        for (XEntity target : targetList) {
+            boolean result = isSatisfiedBy(target);
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isSatisfiedBy(XEntity target) {
+        return true;
+    }
+
+}
