8fdaf423e3593b7fb8de1450e57bfa6eec9f47f4
kakeru.k <kakl2612@gmail.com>
2019-10-30 17:48:42 +0900

    テストコードの修正。

--- sit-cv-tools/src/test/java/io/sitoolkit/cv/tools/infra/config/RepositoryLoggerArgumentParserTest.java
+++ sit-cv-tools/src/test/java/io/sitoolkit/cv/tools/infra/config/RepositoryLoggerArgumentParserTest.java
@@ -14,13 +14,18 @@ public class RepositoryLoggerArgumentParserTest {
              "repositoryMethodMarker=[RepositoryMethod];"
             + "projectType=maven;";
 
-    String filter1 = "repositoryFilter1.annotation=.*(Repository|Named);"
-            + "repositoryFilter1.name=.*Repository.*;";
+    String filter1 = "include.repositoryFilter1.annotation=.*(Repository|Named);"
+            + "include.repositoryFilter1.name=.*Repository.*;";
 
-    String filter2 = "repositoryFilter2.annotation=;"
-            + "repositoryFilter2.name=aaaaa;"
-            + "repositoryFilter3.annotation=bbbbb;"
-            + "repositoryFilter3.name=;";
+    String filter2 = "include.repositoryFilter2.annotation=;"
+            + "include.repositoryFilter2.name=aaaaa;"
+            + "include.repositoryFilter3.annotation=bbbbb;"
+            + "include.repositoryFilter3.name=;";
+
+    String filter3 = "exclude.repositoryFilter1.annotation=;"
+            + "exclude.repositoryFilter1.name=ccccc;"
+            + "exclude.repositoryFilter2.annotation=ddddd;"
+            + "exclude.repositoryFilter2.name=;";
 
     @Test
     public void testParseBasic() throws MalformedURLException {
@@ -31,7 +36,7 @@ public class RepositoryLoggerArgumentParserTest {
         assertThat(filter.getInclude().get(0).getNamePattern().getPattern().toString(), is(".*Repository.*"));
         assertThat(filter.getInclude().get(0).getAnnotationPattern().getPattern().toString(),
                 is(".*(Repository|Named)"));
-        assertThat(filter.getExclude(), is(nullValue()));
+        assertThat(filter.getExclude().size(), is(0));
         assertThat(config.getRepositoryMethodMarker(), is("[RepositoryMethod]"));
         assertThat(config.getProjectType(), is("maven"));
     }
@@ -42,13 +47,14 @@ public class RepositoryLoggerArgumentParserTest {
 
         FilterConditionGroup filter = config.getRepositoryFilter();
         assertThat(filter.getInclude().size(), is(0));
-        assertThat(filter.getExclude(), is(nullValue()));
+        assertThat(filter.getExclude().size(), is(0));
         assertThat(config.getRepositoryMethodMarker(), is("[RepositoryMethod]"));
     }
 
     @Test
     public void testParseManyFilter() throws MalformedURLException {
-        RepositoryLoggerConfig config = new RepositoryLoggerArgumentParser().parse(filter1 + filter2 + base);
+        RepositoryLoggerConfig config = new RepositoryLoggerArgumentParser()
+            .parse(filter1 + filter2 + filter3 + base);
 
         FilterConditionGroup filter = config.getRepositoryFilter();
         assertThat(filter.getInclude().size(), is(3));
@@ -60,7 +66,12 @@ public class RepositoryLoggerArgumentParserTest {
         assertThat(filter.getInclude().get(2).getNamePattern().isEmpty(), is(true));
         assertThat(filter.getInclude().get(2).getAnnotationPattern().getPattern().toString(),
                 is("bbbbb"));
-        assertThat(filter.getExclude(), is(nullValue()));
+        assertThat(filter.getExclude().size(), is(2));
+        assertThat(filter.getExclude().get(0).getNamePattern().getPattern().toString(), is("ccccc"));
+        assertThat(filter.getExclude().get(0).getAnnotationPattern().isEmpty(), is(true));
+        assertThat(filter.getExclude().get(1).getNamePattern().isEmpty(), is(true));
+        assertThat(filter.getExclude().get(1).getAnnotationPattern().getPattern().toString(),
+            is("ddddd"));
         assertThat(config.getRepositoryMethodMarker(), is("[RepositoryMethod]"));
     }
 
