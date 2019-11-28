68689949531991288824981887b42b2d1a282443
kumakuratk <takuya.kumakura@monocrea.jp>
2019-11-28 11:57:00 +0900

    Add entrypoint to home

--- sit-cv-front/src/views/Home.vue
+++ sit-cv-front/src/views/Home.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>Home</div>
+  <v-simple-table dense>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th>EntryPoint</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entryPoint in entryPoints">
+          <td>
+            <router-link :to="entryPoint.url">{{ entryPoint.id }}</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import DesignDocService from '@/domains/designdoc/DesignDocService';
+import DesignDocServiceFactory from '@/domains/designdoc/DesignDocServiceFactory';
+import EntryPoint from '@/domains/designdoc/EntryPoint';
+import MenuItem from '@/domains/designdoc/MenuItem';
 
-export default class Home extends Vue {}
+@Component
+export default class Home extends Vue {
+  designDocService: DesignDocService = DesignDocServiceFactory.getService();
+
+  entryPoints: EntryPoint[] = [];
+
+  created() {
+    this.designDocService.fetchMenuItems((menuItems) => (this.menuItemToEntryPointRecursively(menuItems)));
+  }
+
+  menuItemToEntryPointRecursively(menuItems: MenuItem[]) {
+    menuItems.forEach((menuItem) => {
+      if (menuItem.id == "crud-matrix") {
+        return;
+
+      } else if (menuItem.children.length > 0) {
+        this.menuItemToEntryPointRecursively(menuItem.children);
+
+      } else if (menuItem.endpoint != null) {
+        var entryPointId = menuItem.id.substr(0, menuItem.id.lastIndexOf("."));
+        this.entryPoints.push(new EntryPoint(entryPointId, menuItem.endpoint));
+      }
+    });
+  }
+}
 </script>
